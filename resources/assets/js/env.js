25a4b24e05dda13c837b2ba86f8e56a3ea580f3a
vital <373357042@qq.com>
2017-02-05 22:56:55 +0800

    check

--- resources/assets/js/env.js
+++ resources/assets/js/env.js
@@ -2,5 +2,5 @@
  * Created by Ian Vital <373357042@qq.com> on 2017/1/17 0017.
  */
 export const clientId = 2
-export const clientSecret = '12dW7bzuGM1UVGv8ov1SCrhN0whNHPxx2jJrFgiH'
+export const clientSecret = 'EMMwyKaIDGvezm1a8TVL3JZWLF6Oi6tdcqZztScn'
 export const display_per_page = 15
