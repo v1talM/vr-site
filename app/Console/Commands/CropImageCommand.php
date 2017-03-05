97d5837385603c7af75192216fef0407b3e5099b
v1talM <373357042@qq.com>
2017-03-05 18:48:28 +0800

    调整image:crop命令内容

--- app/Console/Commands/CropImageCommand.php
+++ app/Console/Commands/CropImageCommand.php
@@ -45,27 +45,31 @@ class CropImageCommand extends Command
      */
     public function handle()
     {
-        Log::info('1');
-//        $university = $this->university->where('is_craw', '=', 1)->where('is_crop', '=', 0)->first();
-//        $thumb = $university->thumburl;
-//        $img = Image::make($thumb);
-//        $directory = 'uploads/img/'. $university->pano_id;
-//        $public_path = public_path($directory);
-//        $file_name = "/thumb_" . $university->pano_id . '.jpg';
-//        $file = $public_path.$file_name;
-//        try{
-//            $img->save($file, 90);
-//        }catch (ImageException $e){
-//            $this->info($e->getMessage());
-//            return ;
-//        }
-//        $university->is_crop = true;
-//        $university->save();
-//        $product = Product::create([
-//            'pro_title' => $university->name,
-//            'pro_thumb' => $directory.$file_name,
-//            'pro_photo' => $university->original,
-//            'user_id' => 1
-//        ]);
+        $university = $this->university->where('is_craw', '=', 1)->where('is_crop', '=', 0)->first();
+        $thumb = $university->thumburl;
+        $img = Image::make($thumb);
+        $directory = 'uploads/img/'. $university->pano_id;
+        $public_path = public_path($directory);
+        $file_name = "/thumb_" . $university->pano_id . '.jpg';
+        $file = $public_path.$file_name;
+        try{
+            $img->save($file, 90);
+        }catch (ImageException $e){
+            $this->info($e->getMessage());
+            return ;
+        }
+        $university->is_crop = true;
+        $university->save();
+        try{
+            $product = Product::create([
+                'pro_title' => $university->name,
+                'pro_thumb' => $directory.$file_name,
+                'pro_photo' => $university->original,
+                'user_id' => 1
+            ]);
+            $this->info('id: '. $product->id.' inserted');
+        }catch (\Exception $e){
+            Log::info($e->getMessage());
+        }
     }
 }
