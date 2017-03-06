1b75f4aabbf6dcfba574bf0bc36474e71ae35c21
v1talM <373357042@qq.com>
2017-03-06 15:24:44 +0800

    crop thumb resize

--- app/Console/Commands/CropImageCommand.php
+++ app/Console/Commands/CropImageCommand.php
@@ -53,6 +53,9 @@ class CropImageCommand extends Command
         $file_name = "/thumb_" . $university->pano_id . '.jpg';
         $file = $public_path.$file_name;
         try{
+            $img->resize(null, 310, function ($constraint) {
+                $constraint->aspectRatio();
+            });
             $img->save($file, 90);
         }catch (ImageException $e){
             $this->info($e->getMessage());
--- resources/assets/js/env.js
+++ resources/assets/js/env.js
@@ -2,11 +2,7 @@
  * Created by Ian Vital <373357042@qq.com> on 2017/1/17 0017.
  */
 export const clientId = 2
-<<<<<<< HEAD
-export const clientSecret = 'EMMwyKaIDGvezm1a8TVL3JZWLF6Oi6tdcqZztScn'
-export const display_per_page = 15
-=======
 export const clientSecret = 'Gc4BzGNJcS7Z2hxjFB3w0EDxxuAfnz4BUIriTMp5'
 export const display_per_page = 15
 export const featuredPerPage = 4
->>>>>>> e7a27161d49375aaaa02738c19798410542aa08d
+
