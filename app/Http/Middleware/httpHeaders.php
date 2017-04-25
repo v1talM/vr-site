08bec7d50c11841224e2ba5ac3c6a626384eac0c
v1talM <373357042@qq.com>
2017-04-25 10:18:33 +0800

    cors

--- app/Http/Middleware/httpHeaders.php
+++ app/Http/Middleware/httpHeaders.php
@@ -16,7 +16,7 @@ class httpHeaders
     public function handle($request, Closure $next)
     {
         return $next($request)->header('Access-Control-Allow-Origin' , '*')
-            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With');
+            ->header('Access-Control-Allow-Methods', '*')
+            ->header('Access-Control-Allow-Headers', '*');
     }
 }
